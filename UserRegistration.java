49f1d99fac49e9efcc700dd2d76aab5243fa61d7
Aishwarya <amuthyala511@gmail.com>
2020-10-22 18:53:09 +0530

    Validating first name

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,8 +1,20 @@
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 public class UserRegistration
 {
 	public static void main(String[] args)
 	{
 		System.out.println("WELCOME TO USER REGISTRATION PAGE");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter first name: ");
+		String FirstName = sc.nextLine();
+		String regexPattern = "^[A-Z][a-z]{2,}$";
+		Pattern p = Pattern.compile(regexPattern);
+		Matcher m = p.matcher(FirstName);
+		if(m.matches())
+			System.out.println("First Name is Valid");
+		else
+			System.out.println("First Name is Invalid");
 	}
 }
