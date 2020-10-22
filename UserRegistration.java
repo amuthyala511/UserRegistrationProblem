95068039f90ab30a8813320a502d2f1929771488
Aishwarya <amuthyala511@gmail.com>
2020-10-22 19:35:38 +0530

    Validating last name along with first name

--- UserRegistration.java
+++ UserRegistration.java
@@ -8,13 +8,30 @@ public class UserRegistration
 		System.out.println("WELCOME TO USER REGISTRATION PAGE");
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter first name: ");
-		String FirstName = sc.nextLine();
+		String firstname = sc.nextLine();
+		System.out.println("Enter last name: ");
+		String lastname = sc.nextLine();
+		validateFirstName(firstname);
+		validateLastName(lastname);
+	}
+	static void validateFirstName(String firstname)
+	{
 		String regexPattern = "^[A-Z][a-z]{2,}$";
 		Pattern p = Pattern.compile(regexPattern);
-		Matcher m = p.matcher(FirstName);
+		Matcher m = p.matcher(firstname);
 		if(m.matches())
-			System.out.println("First Name is Valid");
+			System.out.println("Valid first name");
 		else
-			System.out.println("First Name is Invalid");
+			System.out.println("Invalid first name");
 	}
+	static void validateLastName(String lastname)
+   {
+      String regexPattern = "^[A-Z][a-z]{2,}$";
+      Pattern p = Pattern.compile(regexPattern);
+      Matcher m = p.matcher(lastname);
+      if(m.matches())
+         System.out.println("Valid last name");
+      else
+         System.out.println("Invalid last name");
+   }
 }
